222ae82e84a6709656617f882019053edbaafc35
Mariana Ramos <mary.programms@gmail.com>
2023-12-24 22:44:12 +0000

    counter exercise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const increaseCount = () => {
+    setCount(count + 1)
+  }
+  const decreaseCount = () => {
+    setCount(count - 1)
+  }
+  const setCountToZero = () => {
+    setCount(0)
+  }
   return (
-    <div className="App">New project change!</div>
-  );
+    <div className="App">
+      <button onClick={increaseCount}>Increase</button>
+      <br></br>
+      <button onClick={decreaseCount}>Decrease</button>
+      <br></br>
+      <button onClick={setCountToZero}>Set to Zero</button>
+
+      {count}
+    </div>
+  )
 }
 
 export default App;
